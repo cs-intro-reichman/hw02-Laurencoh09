c02233475c15065af679dbb821c0eb70c2ce565c
Lauren Cohen <154359581+Laurencoh09@users.noreply.github.com>
2024-01-03 20:57:16 +0200

    Update InOrder.java

--- InOrder.java
+++ InOrder.java
@@ -1,15 +1,14 @@
  import java.util.Random;
-  
+
+
 public class  InOrder {
    	public static void main (String[] args) {
 
    	    int num1 = -1;
    		int num2 = 0;
    		String res = "";
-
    		do{
    			num2 = (int)(Math.random() * 10);
-
             if( num2 >= num1 ){
         	 
         	  res = res + num2 + " ";
@@ -19,11 +18,10 @@ public class  InOrder {
             else {
             	break;
             }
-
-        
         }
         while( true );
-
         System.out.println(res);
+
+     
     }
 }
