2f89d4a4d2065383d64df3141ac61825dc10ccb0
Lauren Cohen <154359581+Laurencoh09@users.noreply.github.com>
2024-01-03 13:19:54 +0200

    Update Perfect.java

--- Perfect.java
+++ Perfect.java
@@ -1,8 +1,32 @@
 /**
  *  Gets a command-line argument (int), and chekcs if the given number is perfect.
  */
-public class Perfect {
-	public static void main (String[] args) {
-		//// Put your code here
-	}
+
+public class  Perfect {
+   	public static void main (String[] args) {
+        int x = Integer.parseInt(args[0]);
+if (x < 0) {
+ System.out.println(x + " is not a perfect number ");
 }
+
+String res = x + " is a prefect number since " + x + " = 1"; 
+
+int counter = 1;
+   		for (int i = 2; i < x-1; i++) {
+            if ( x % i == 0 ) {
+counter = counter + i;
+res = res+ " + " +i;
+    }
+}
+if (counter == x){
+System.out.println(res);
+} 
+else {
+        System.out.println(x + " is not a perfect number ");
+
+   		}
+   	}
+
+   		}
+     
+ 		
