ae98c7aeb48bffd37d203f3e89ec12653e4e97eb
Lauren Cohen <154359581+Laurencoh09@users.noreply.github.com>
2024-01-03 20:40:12 +0200

    Update InOrder.java

--- InOrder.java
+++ InOrder.java
@@ -1,10 +1,7 @@
-/**
+ import java.util.Random;
  *  Generates and prints random integers in the range [0,10),
  *  as long as they form a non-decreasing sequence.
  */
-import java.util.Random;
-
-
 public class  InOrder {
    	public static void main (String[] args) {
 
