ad5587c0f8a3e9be4cc87f9d9a8c03fb1a1302ea
Lauren Cohen <154359581+Laurencoh09@users.noreply.github.com>
2024-01-03 21:34:45 +0200

    Update OneOfEachStats1.java

--- OneOfEachStats1.java
+++ OneOfEachStats1.java
@@ -6,20 +6,21 @@
  */
 
 
-public class OneOfEachStats1{
-   	public static void main (String[] args){
+public class OneOfEachStats1 {
+   	public static void main (String[] args) {
 
+		
 		int T = Integer.parseInt(args[0]);
 		int num1 = 0, num2 = 0, num3 = 0, num4 = 0;
-		
+
 		for (int i = 1; i <= T; i++) {
 		int children = 0;
    		boolean boy = false; 
    		boolean girl = false;
 
    		while(!boy || !girl ){ 
+   			
    double random = Math.random();
-			
    		if (random <= 0.5){
    			boy = true;	 
    		}
@@ -37,21 +38,21 @@ children ++;
 			}
 			num1 += children;
 		}
-		double average = num1 / T;
+double average = num1 / T;
 		System.out.println("Average: " + average + " children to get at least one of each gender.");
-
-		                  System.out.println("Number of families with 2 children: " + num2);
-		                  
+        System.out.println("Number of families with 2 children: " + num2);
+		
 		System.out.println("Number of families with 3 children: " + num3);
 		                   System.out.println("Number of families with 4 children: " + num4);
-		if (num2 > num3) {
+if (num2 > num3) {
 
 			               System.out.println("The most common number of children is 2.");
-		} else if (num3 > num4) {
+} else if (num3 > num4) {
 
 			                 System.out.println("The most common number of children is 3.");
-		} else {
+} else {
 			                System.out.println("The most common number of children is 4 or more.");
 		}
+		
 	}
 }
