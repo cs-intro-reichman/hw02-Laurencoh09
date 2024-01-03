66a12400582522107736d7934b88c55953ed4d22
Lauren Cohen <154359581+Laurencoh09@users.noreply.github.com>
2024-01-03 21:02:40 +0200

    Update Perfect.java

--- Perfect.java
+++ Perfect.java
@@ -3,9 +3,10 @@
  */
 
 
-public class Perfect{
-   	public static void main (String[] args){
+public class Perfect {
+   	public static void main (String[] args) {
         int x = Integer.parseInt(args[0]);
+         
 if (x < 0) {
  System.out.println(x + " is not a perfect number ");
 }
